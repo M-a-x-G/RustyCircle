6fc828b10242da219b3e45ed3844bd9e700d34d9
Max Gregor <Gregor@fh-brandenburg.de>
2015-10-21 11:35:39 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,11 @@ extern crate rustc_serialize;
 use docopt::Docopt;
 use imagefmt::{ColFmt, ColType};
 
+/// Main function called on excecution
+
 fn main() {
 
+	// usage string for comandline help
 	const USAGE: &'static str = "
 Usage:
  rusty_circle <center-x> <center-y> <radius>
@@ -33,6 +36,7 @@ Options:
     -h, --help         Show this message.
 ";
 
+	// Struct for comandline values
 	#[derive(RustcDecodable)]
 	struct Args {
 		arg_center_x: String,
@@ -45,40 +49,53 @@ Options:
 	.and_then(|d| d.decode())
 	.unwrap_or_else(|e| e.exit());
 
-	let radius = args.arg_radius;
-	let cx = args.arg_center_x;
-	let cy = args.arg_center_y;
-	let cxnum: i32;
-	let cynum: i32;
-	let radiusnum: i32;
+	let redius_str = args.arg_radius;
+	let cx_str = args.arg_center_x;
+	let cy_str = args.arg_center_y;
+	// center x coordinate of the circle
+	let cx: i32;
+	// center y coordinate of the circle
+	let cy: i32;
+	// center radius of the circle
+	let radius: i32;
+	// height in pixel of the outputimage
 	let h = 901;
+	// width in pixel of the outputimage
 	let w = 1201;
+	// vector for image generation where one pixel is represented by three following entries (Red, Green, Blue).
 	let mut image_vector: Vec<u8> = vec![255;(3 * w * h)];
 	let w3: i32 = (w * 3) as i32;
 
-	cxnum = match cx.trim().parse(){
+	// parse the center x coordinate
+	cx = match cx_str.trim().parse(){
 						Ok(num) => if num > 0 && num <= 150 {num} else {println!("Please type a y coordinate between 1-150!"); -1},
 						Err (_) => {println!("Please type a x coordinate between 1-150!"); return}, };
 
-	radiusnum = match radius.trim().parse(){
-						Ok(num) => if num > 0 && num < 100{num} else {println!("Please type a radius between 1-100!"); -1},
+	// parse the radius
+	radius = match redius_str.trim().parse(){
+						Ok(num) => if num > 0 && num <= 100{num} else {println!("Please type a radius between 1-100!"); -1},
 						Err (_) => {println!("Please type a number between 1-100!e"); return}, };
 
-	cynum = match cy.trim().parse(){
+	// parse the center y coordinate
+	cy = match cy_str.trim().parse(){
 						Ok(num) => if num > 0 && num <= 200 {num} else {println!("Please type a y coordinate between 1-200!"); -1},
 						Err (_) => {println!("Please type a y coordinate between 1-200!"); return}, };
 
-	draw_grid(w3, &mut image_vector);
-	draw_circle(cxnum, cynum, radiusnum, w3, &mut image_vector);
+	prepare_grid(w3, &mut image_vector);
+	prepare_circle(cx, cy, radius, w3, &mut image_vector);
 
 	//  write image
 	imagefmt::Image{w: w, h: h, fmt:ColFmt::RGB, buf:image_vector}.write("circle.png", ColType::Color).unwrap();
 
-	println!("Circle succussfully saved to circle.png");
+	println!("Circle image succussfully saved to circle.png");
 
 }
 
-fn draw_grid(w3: i32, image_vector: &mut Vec<u8>){
+/// Sets points for a grid in a `Vec<u8>` for image generation where one pixel is represented by three following entries (Red, Green, Blue).
+/// Each line of the grid is one pixel wide. The Grid has a widh of 200 points and a heigth of 150 points where one point is 5px x 5px.
+/// Additionaly the value for three times the overall width of the output image (`w3`) is needed.
+
+fn prepare_grid(w3: i32, image_vector: &mut Vec<u8>){
 	let mut line_counter = 0;
 	for i in 0..image_vector.len() {
 		if i % (w3 as usize) == 0 {
@@ -102,18 +119,22 @@ fn draw_grid(w3: i32, image_vector: &mut Vec<u8>){
 	}
 }
 
-fn draw_circle(cxnum: i32, cynum: i32, radiusnum: i32, w3: i32, image_vector: &mut Vec<u8>) {
-	let mut f: i32 = 1 - radiusnum as i32;
-	let mut dd_f_x: i32 = 0;
-	let mut dd_f_y: i32 = -2 * radiusnum as i32;
-	let mut x: i32 = 0;
-	let mut y: i32 = radiusnum;
+/// Sets points for a circle with given x (`cx`), y (`cy`) as center coordinates and a radius (`radius`) in a `Vec<u8>` for image generation where
+/// one pixel is represented by three following entries (Red, Green, Blue).
+/// Additionaly the value for three times the overall width of the output image (`w3`) is needed.
 
-	// Draw right, left, top and bottom point
-	set_point(cxnum, cynum + radiusnum, image_vector, w3);
-	set_point(cxnum, cynum - radiusnum, image_vector, w3);
-	set_point(cxnum + radiusnum, cynum, image_vector, w3);
-	set_point(cxnum - radiusnum, cynum, image_vector, w3);
+fn prepare_circle(cx: i32, cy: i32, radius: i32, w3: i32, image_vector: &mut Vec<u8>) {
+	let mut f: i32 = 1 - radius as i32;
+	let mut dd_f_x: i32 = 0;
+	let mut dd_f_y: i32 = -2 * radius as i32;
+	let mut x: i32 = 0;
+	let mut y: i32 = radius;
+
+	// Set right, left, top and bottom point
+	set_point(cx, cy + radius, image_vector, w3);
+	set_point(cx, cy - radius, image_vector, w3);
+	set_point(cx + radius, cy, image_vector, w3);
+	set_point(cx - radius, cy, image_vector, w3);
 
 	while x < y {
 		if f >= 0 {
@@ -125,16 +146,20 @@ fn draw_circle(cxnum: i32, cynum: i32, radiusnum: i32, w3: i32, image_vector: &m
 		dd_f_x += 2;
 		f += dd_f_x + 1;
 
-		set_point(cxnum + x, cynum + y, image_vector, w3);
-		set_point(cxnum - x, cynum + y, image_vector, w3);
-		set_point(cxnum + x, cynum - y, image_vector, w3);
-		set_point(cxnum - x, cynum - y, image_vector, w3);
-		set_point(cxnum + y, cynum + x, image_vector, w3);
-		set_point(cxnum - y, cynum + x, image_vector, w3);
-		set_point(cxnum + y, cynum - x, image_vector, w3);
-		set_point(cxnum - y, cynum - x, image_vector, w3);
+		set_point(cx + x, cy + y, image_vector, w3);
+		set_point(cx - x, cy + y, image_vector, w3);
+		set_point(cx + x, cy - y, image_vector, w3);
+		set_point(cx - x, cy - y, image_vector, w3);
+		set_point(cx + y, cy + x, image_vector, w3);
+		set_point(cx - y, cy + x, image_vector, w3);
+		set_point(cx + y, cy - x, image_vector, w3);
+		set_point(cx - y, cy - x, image_vector, w3);
 	}
 }
+
+/// Sets a point to black (0, 0, 0) with given `x` and `y` coordinates in a `Vec<u8>` for image generation where
+/// one pixel is represented by three following entries (Red, Green, Blue).
+/// Additionaly the value for three times the overall width of the output image (`w3`) is needed.
 
 fn set_point(x: i32, y: i32, image_vector: &mut Vec<u8>, w3: i32){
 	if x<=200 && y<=150 && x > 0 && y > 0  {
